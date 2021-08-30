a3f109f2514c66ca37932817ec7babfbbff5b812
Sawwaty <yezhovye@mac.com>
2021-08-31 00:10:27 +0300

    Presentation app: UI elements are setup

---
+++ AppPresentation/ContentViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ContentViewController.swift
+//  AppPresentation
+//
+//  Created by Victoria Klimova on 31.08.2021.
+//
+
+import UIKit
+
+class ContentViewController: UIViewController {
+
+    @IBOutlet weak var presentTextLabel: UILabel!
+    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var pageControl: UIPageControl!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ AppPresentation/PageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PageViewController.swift
+//  AppPresentation
+//
+//  Created by Victoria Klimova on 31.08.2021.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
